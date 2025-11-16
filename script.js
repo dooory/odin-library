c31f8271817d9cea7831ecc4be368ad9643b95cb
Dory <78720364+dooory@users.noreply.github.com>
2025-11-16 13:43:47 +1300

    add addBookToPage()

--- script.js
+++ script.js
@@ -10,20 +10,40 @@ function addBookToLibrary(book) {
 	myLibrary[book.id] = book;
 }
 
-let FantasyBook = new Book("My Fantasy Book", "Fantasy");
-let HorrorBook = new Book("My Horror Book", "Horror");
-let ActionBook = new Book("My Action Book", "Action");
-let ScfiBook = new Book("My Sc-fi Book", "Sc-fi");
-let AdventureBook = new Book("My Adventure Book", "Adventure");
+const booksContainer = document.querySelector(".books-container");
+
+function addBookToPage(book) {
+	const bookElement = document.createElement("div");
+	bookElement.className = "book";
+
+	const title = document.createElement("h1");
+	const genre = document.createElement("p");
+
+	title.textContent = book.name;
+	genre.textContent = book.genre;
+
+	bookElement.appendChild(title);
+	bookElement.appendChild(genre);
+
+	booksContainer.appendChild(bookElement);
+}
+
+const FantasyBook = new Book("My Fantasy Book", "Fantasy");
+const HorrorBook = new Book("My Horror Book", "Horror");
+const ActionBook = new Book("My Action Book", "Action");
+const ScfiBook = new Book("My Sc-fi Book", "Sc-fi");
+const AdventureBook = new Book("My Adventure Book", "Adventure");
+const RomanceBook = new Book("My Romance Book", "Romance");
 
 addBookToLibrary(FantasyBook);
 addBookToLibrary(HorrorBook);
 addBookToLibrary(ActionBook);
 addBookToLibrary(ScfiBook);
 addBookToLibrary(AdventureBook);
+addBookToLibrary(RomanceBook);
 
-for (const book in myLibrary) {
-	const element = myLibrary[book];
+for (const bookId in myLibrary) {
+	const book = myLibrary[bookId];
 
-	console.log(element);
+	addBookToPage(book);
 }
