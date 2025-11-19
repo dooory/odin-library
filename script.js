39e7cc8672b444be4bd96e37931eb1001df00a5b
Dory <78720364+dooory@users.noreply.github.com>
2025-11-20 11:55:43 +1300

    add creating new books

--- script.js
+++ script.js
@@ -1,9 +1,71 @@
 let myLibrary = [];
 
 const booksContainer = document.querySelector(".books-container");
+
 const createBookModal = document.querySelector(".create-book-modal");
+const createBookForm = document.getElementById("create-book-form");
+
 const openModalButton = document.querySelector(".open-modal-button");
 const closeModalButton = document.querySelector(".close-modal-button");
+const addBookButton = document.getElementById("add-book");
+
+function Book(data) {
+	Object.assign(this, data);
+	this.id = crypto.randomUUID();
+}
+
+function addBookToPage(data) {
+	const book = document.createElement("div");
+	book.classList.add("book");
+
+	if (data.hasRead) {
+		book.classList.add("has-read-book");
+	}
+
+	const bookHeader = document.createElement("div");
+	const bookDescription = document.createElement("div");
+
+	bookHeader.classList.add("book-header");
+	bookDescription.classList.add("book-description");
+
+	const title = document.createElement("h1");
+	const author = document.createElement("h3");
+	const genre = document.createElement("p");
+	const pageCount = document.createElement("p");
+
+	title.classList.add("book-title");
+	author.classList.add("book-author");
+	genre.classList.add("book-genre");
+	pageCount.classList.add("book-page-count");
+
+	title.textContent = data.title;
+	author.textContent = `By ${data.author}`;
+	pageCount.textContent = `Pages: ${data.pageCount}`;
+	genre.textContent = `Genre: ${data.genre}`;
+
+	bookHeader.appendChild(title);
+	bookHeader.appendChild(author);
+	bookDescription.appendChild(genre);
+	bookDescription.appendChild(pageCount);
+
+	book.appendChild(bookHeader);
+	book.appendChild(bookDescription);
+	booksContainer.appendChild(book);
+}
+
+function addBookToLibrary(book) {
+	myLibrary[book.id] = book;
+}
+
+const FantasyBook = new Book({
+	title: "My Fantasy Book",
+	author: "Gabriela Day",
+	genre: "Fantasy",
+	pageCount: 100,
+	hasRead: true,
+});
+
+console.log(FantasyBook);
 
 openModalButton.addEventListener("click", (event) =>
 	createBookModal.showModal()
@@ -11,56 +73,19 @@ openModalButton.addEventListener("click", (event) =>
 
 closeModalButton.addEventListener("click", (event) => createBookModal.close());
 
-function Book(name, author, genre, pages, hasRead) {
-	this.name = name;
-	this.author = author;
-	this.genre = genre;
-	this.pages = pages;
-	this.read = hasRead;
-	this.id = crypto.randomUUID();
-}
+createBookForm.addEventListener("submit", (event) => {
+	const formData = new FormData(createBookForm, addBookButton);
+	const bookData = Object.fromEntries(formData);
 
-function addBookToLibrary(book) {
-	myLibrary[book.id] = book;
-}
+	const newBook = new Book(bookData);
 
-function addBookToPage(book) {
-	const bookElement = document.createElement("div");
-	bookElement.className = "book";
+	addBookToLibrary(newBook);
+	addBookToPage(newBook);
 
-	const title = document.createElement("h1");
-	const genre = document.createElement("p");
-
-	title.textContent = book.name;
-	genre.textContent = book.genre;
-
-	bookElement.appendChild(title);
-	bookElement.appendChild(genre);
-
-	booksContainer.appendChild(bookElement);
-}
-
-const FantasyBook = new Book(
-	"My Fantasy Book",
-	"Gabriela Day",
-	"Fantasy",
-	100,
-	false
-);
-
-const HorrorBook = new Book(
-	"My Horror Book",
-	"Julie Lawson",
-	"Horror",
-	200,
-	true
-);
-
-const ActionBook = new Book("My Action Book", "Pedro Lam", "Action", 300, true);
+	createBookForm.reset();
+});
 
 addBookToLibrary(FantasyBook);
-addBookToLibrary(HorrorBook);
-addBookToLibrary(ActionBook);
 
 for (const bookId in myLibrary) {
 	const book = myLibrary[bookId];
