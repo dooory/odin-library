6a6212ad40221ed1734b64379d0b5b5a0095a147
Dory <78720364+dooory@users.noreply.github.com>
2025-11-20 12:36:19 +1300

    refactor Book function towards oop

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
-let myLibrary = [];
+let myLibrary = {
+	name: "MyLibrary",
+	books: [],
+};
 
 const booksContainer = document.querySelector(".books-container");
 
@@ -12,89 +15,71 @@ const addBookButton = document.getElementById("add-book");
 function Book(data) {
 	Object.assign(this, data);
 	this.id = crypto.randomUUID();
+	this.libraries = [];
+
+	this.addToLibrary = function (library) {
+		this.libraries.push(library);
+		library.books[this.id] = this;
+	};
+
+	this.addToLibraryPage = function (library) {
+		const book = document.createElement("div");
+		book.classList.add("book");
+		book.dataset.bookId = this.id;
+
+		this.element = book;
+
+		if (this.hasRead) {
+			book.classList.add("has-read-book");
+		}
+
+		const bookHeader = document.createElement("div");
+		const bookDescription = document.createElement("div");
+
+		bookHeader.classList.add("book-header");
+		bookDescription.classList.add("book-description");
+
+		const title = document.createElement("h1");
+		const author = document.createElement("h3");
+		const genre = document.createElement("p");
+		const pageCount = document.createElement("p");
+
+		const deleteButton = document.createElement("button");
+		deleteButton.classList.add("delete-book-button");
+		deleteButton.textContent = "X";
+
+		title.classList.add("book-title");
+		author.classList.add("book-author");
+		genre.classList.add("book-genre");
+		pageCount.classList.add("book-page-count");
+
+		title.textContent = this.title;
+		author.textContent = `By ${this.author}`;
+		pageCount.textContent = `Pages: ${this.pageCount}`;
+		genre.textContent = `Genre: ${this.genre}`;
+
+		bookHeader.appendChild(deleteButton);
+		bookHeader.appendChild(title);
+		bookHeader.appendChild(author);
+		bookDescription.appendChild(genre);
+		bookDescription.appendChild(pageCount);
+
+		book.appendChild(bookHeader);
+		book.appendChild(bookDescription);
+		booksContainer.appendChild(book);
+
+		deleteButton.addEventListener("click", () => {
+			this.removeFromLibrary(library);
+			this.removeFromLibraryPage(library);
+		});
+	};
+
+	this.removeFromLibraryPage = function () {
+		this.element.remove();
+	};
+
+	this.removeFromLibrary = function () {};
 }
-
-function addBookToPage(data) {
-	const book = document.createElement("div");
-	book.classList.add("book");
-	book.dataset.bookId = data.id;
-
-	if (data.hasRead) {
-		book.classList.add("has-read-book");
-	}
-
-	const bookHeader = document.createElement("div");
-	const bookDescription = document.createElement("div");
-
-	bookHeader.classList.add("book-header");
-	bookDescription.classList.add("book-description");
-
-	const title = document.createElement("h1");
-	const author = document.createElement("h3");
-	const genre = document.createElement("p");
-	const pageCount = document.createElement("p");
-
-	const deleteButton = document.createElement("button");
-	deleteButton.classList.add("delete-book-button");
-	deleteButton.textContent = "X";
-
-	title.classList.add("book-title");
-	author.classList.add("book-author");
-	genre.classList.add("book-genre");
-	pageCount.classList.add("book-page-count");
-
-	title.textContent = data.title;
-	author.textContent = `By ${data.author}`;
-	pageCount.textContent = `Pages: ${data.pageCount}`;
-	genre.textContent = `Genre: ${data.genre}`;
-
-	bookHeader.appendChild(deleteButton);
-	bookHeader.appendChild(title);
-	bookHeader.appendChild(author);
-	bookDescription.appendChild(genre);
-	bookDescription.appendChild(pageCount);
-
-	book.appendChild(bookHeader);
-	book.appendChild(bookDescription);
-	booksContainer.appendChild(book);
-
-	deleteButton.addEventListener("click", () => {
-		removeBookFromLibrary(data.id);
-		removeBookFromPage(data.id);
-	});
-}
-
-function addBookToLibrary(book) {
-	myLibrary[book.id] = book;
-}
-
-function removeBookFromLibrary(bookId) {
-	if (!myLibrary[bookId]) {
-		console.error(`No book with id <${bookId}> in Library`);
-
-		return;
-	}
-
-	delete myLibrary[bookId];
-}
-
-function removeBookFromPage(bookId) {
-	const book = document.querySelector(`[data-book-id='${bookId}']`);
-
-	if (!book) {
-		console.error(`No book element with id <${bookId} found`);
-	}
-
-	book.remove();
-}
-
-const FantasyBook = new Book({
-	title: "My Fantasy Book",
-	author: "Gabriela Day",
-	genre: "Fantasy",
-	pageCount: 100,
-	hasRead: true,
-});
 
 openModalButton.addEventListener("click", (event) =>
 	createBookModal.showModal()
@@ -108,16 +93,19 @@ createBookForm.addEventListener("submit", (event) => {
 
 	const newBook = new Book(bookData);
 
-	addBookToLibrary(newBook);
-	addBookToPage(newBook);
+	newBook.addToLibrary(myLibrary);
+	newBook.addToLibraryPage();
 
 	createBookForm.reset();
 });
 
-addBookToLibrary(FantasyBook);
+const FantasyBook = new Book({
+	title: "My Fantasy Book",
+	author: "Gabriela Day",
+	genre: "Fantasy",
+	pageCount: 100,
+	hasRead: true,
+});
 
-for (const bookId in myLibrary) {
-	const book = myLibrary[bookId];
-
-	addBookToPage(book);
-}
+FantasyBook.addToLibrary(myLibrary);
+FantasyBook.addToLibraryPage(myLibrary);
